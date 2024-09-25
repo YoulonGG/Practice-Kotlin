86be9cfe4579cfe67a6fc995914e92194fb617f9
YoulonGG <johnyoulong@gmail.com>
2024-09-25 11:52:36 +0700

    Initial commit

--- app/src/main/java/com/example/practicekotlin/Scope2Activity.kt
+++ app/src/main/java/com/example/practicekotlin/Scope2Activity.kt
@@ -1,6 +1,10 @@
 package com.example.practicekotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +16,25 @@ class Scope2Activity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_scope2)
 
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmitScope)
+        val btnClear = findViewById<Button>(R.id.btnClearScope)
+        val txtUsername = findViewById<EditText>(R.id.txtScopeName)
+        val txtResult = findViewById<TextView>(R.id.txtResultScope)
 
+        txtUsername.text = null
 
+        btnSubmit.setOnClickListener {
+            txtResult.text = txtUsername.text
+        }
 
+        btnClear.setOnClickListener {
+            txtResult.text = ""
+            txtUsername.text.clear()
+        }
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
